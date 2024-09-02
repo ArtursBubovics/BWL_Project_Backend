980701692b487abeb164ec98ef0dd2867f11edae
ArtursBubovics <artursbubovocs@gmail.com>
2024-09-02 21:52:55 +0300

    Added Xsrf token endpoint

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,6 +11,11 @@ Route::get('/', function () {
 
 Route::get('/csrf-token', function () {
     return response()->json(['csrf_token' => csrf_token()]);
+});
+
+Route::get('/xsrf-token', function (Request $request) {
+    $xsrfToken = $request->cookie('XSRF-TOKEN');
+    return response()->json(['xsrf_token' => $xsrfToken]);
 });
 
 Route::post('/register', [AuthController::class, 'register']);
