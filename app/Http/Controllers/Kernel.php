4501e8ec35bf0f1978802fdfced08b574b02468d
ArtursBubovics <artursbubovocs@gmail.com>
2024-09-02 11:24:44 +0300

    Fixed the error with CORS

--- app/Http/Controllers/Kernel.php
+++ app/Http/Controllers/Kernel.php
@@ -3,5 +3,6 @@ protected $middlewareGroups = [
     \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
     'throttle:api',
     \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    \Fruitcake\Cors\HandleCors::class,
 ],
 ];
