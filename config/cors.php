3ff308242ee20654525c9e02130db46a5cd37186
ArtursBubovics <artursbubovocs@gmail.com>
2024-09-08 20:52:31 +0300

    Changed the address to http://127.0.0.1

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', '/*', '*'], // Add your routes here
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'], // Update this with the origin of your React app
+    'allowed_origins' => ['http://127.0.0.1:3000'], // Update this with the origin of your React app
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
