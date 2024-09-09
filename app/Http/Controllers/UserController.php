38898e18ca57cb9e7bc7b44c4abd6c931300990c
ArtursBubovics <artursbubovocs@gmail.com>
2024-09-09 13:46:42 +0300

    Added deletion of account users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+use App\Models\RefreshToken;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class UserController extends Controller
 {
@@ -78,6 +80,44 @@ class UserController extends Controller
         $user->save();
 
         return response()->json(['message' => 'User updated successfully'], 200);
+    }
+
+ 
+/**
+ * @OA\Delete(
+ *     path="/api/user/delete",
+ *     summary="Delete the current authenticated user and associated tokens",
+ *     tags={"User"},
+ *     security={{"bearerAuth": {}}},
+ *     @OA\Response(
+ *         response=200,
+ *         description="User deleted successfully",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="message", type="string", example="User deleted successfully")
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=401,
+ *         description="Unauthorized",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="message", type="string", example="Unauthorized")
+ *         )
+ *     )
+ * )
+ */
+    public function delete_user(Request $request)
+    {
+        $user = $request->user();
+
+        PersonalAccessToken::where('tokenable_id', $user->id)
+        ->where('tokenable_type', 'App\Models\User')
+        ->delete();
+
+        RefreshToken::where('user_id', $user->id)->delete();
+
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted successfully'], 200);
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,11 @@ Route::post('/new-access-token', [AuthController::class, 'new_access_token']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+
 Route::middleware('check.token.expiry')->group(function () {
     Route::put('/user/update', [UserController::class, 'user_data_update']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::delete('/user/delete', [UserController::class, 'delete_user']);
 });
